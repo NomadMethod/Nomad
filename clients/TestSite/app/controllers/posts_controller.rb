7a4e5f841a61463fe481ce071c304a66da35424d
Bryan <Bryan@nomadmethod.com>
2015-04-16 18:29:10 -0500

    Created and added some database configuration and what-not.

---
+++ clients/TestSite/app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = 'This is a sample post text stuff'
+        @content_second = 'Second Post YO!'
+    end
+
+
+    def new
+
+    end
+
+
+    def create
+
+    end
+
+    
+    def edit
+
+    end
+
+
+    def update
+
+    end
+
+
+    def show
+
+    end
+
+
+    def destroy
+
+    end
+end
